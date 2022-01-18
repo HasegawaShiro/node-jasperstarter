439e66eba1fc506b4aac166a00e0d09da61d845a
HasegawaShiro <shiraha.soujin@gmail.com>
2022-01-18 17:35:06 +0800

    refactor generator()

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const fs = require('fs'),
+  path = require('path'),
   { spawn } = require('child_process')
 
 module.exports = {
-  async compile() { },
+  async compile(jrxml) { },
   async generate({
+    jrxml,
     jasper,
     output,
     format,
     input,
     type,
   }) {
-    const process = spawn('./jasperstarter/bin/jasperstarter', [
+    const processing = spawn(path.resolve(__dirname, './jasperstarter/bin/jasperstarter'), [
       "process", jasper,
       "-o", output,
       "-f", format,
@@ -18,25 +20,28 @@ module.exports = {
       "--data-file", input
     ])
 
-    process.stdout.on('data', (data) => {
+    processing.stdout.on('data', (data) => {
       if (data) console.log(`stdout: ${data}`);
     })
 
-    process.stderr.on('data', (data) => {
+    processing.stderr.on('data', (data) => {
       if (data) console.error(`stderr: ${data}`)
     })
 
-    process.on('exit', code => {
-      if (code === 0) {
-        const fileName = `${output}.${format}`,
-          exportFile = fs.readFileSync(fileName)
+    const generated = new Promise((resolve, reject) => {
+      processing.on('exit', code => {
+        if (code === 0) {
+          const fileName = `${output}.${format}`,
+            exportFile = fs.readFileSync(fileName)
 
-        fs.unlinkSync(fileName)
-
-        return exportFile
-      } else {
-        throw "error"
-      }
+          fs.unlinkSync(fileName)
+          resolve(exportFile)
+        } else {
+          reject()
+        }
+      })
     })
+
+    return generated
   },
 }
